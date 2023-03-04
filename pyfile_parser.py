625af8ab0bf488e9aecb614e82306784d812da24
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2023-03-04 03:27:05 -0500

    Complete basic functionality for parser

--- pyfile_parser.py
+++ pyfile_parser.py
@@ -1,8 +1,28 @@
-def check_imports(filename):
-    import_lines = []
+def get_lines(filename) -> list:
     with open(filename) as f:
         lines = f.readlines()
-        import_lines = [
-            ('flask' in line or 'Flask' in line) for line in lines if (line.startswith('import') or line.startswith('from'))
-        ]
-    return any(import_lines)
+    return [line.strip() for line in lines]
+
+def check_imports(lines) -> bool:
+    return any([
+        ('flask' in line or 'Flask' in line)
+        for line in lines
+        if (line.startswith('import') or line.startswith('from'))
+    ])
+
+def get_responses(i_lines) -> list:
+    route_starts = [i for (i, line) in i_lines if line.startswith('@app.route')]
+    return_lines = []
+    for j, start in enumerate(route_starts[:-1]):
+        return_lines.append([line for (i, line) in i_lines[start:route_starts[j + 1]] if line.startswith('return')])
+    return_lines.append([line for (i, line) in i_lines[route_starts[-1]:] if line.startswith('return')])
+    if len(return_lines) < 1:
+        return 'No return statements found in file, file is invalid'
+    return [line for line_group in return_lines for line in line_group]
+
+def parse_and_return_responses(filename) -> list:
+    lines = get_lines(filename)
+    if not check_imports(lines):
+        return 'No imports of flask found, file is invalid'
+    indexed_lines = [(i, line) for i, line in enumerate(lines)]
+    return get_responses(indexed_lines)
--- test_main.py
+++ test_main.py
@@ -1,5 +1,9 @@
-from pyfile_parser import check_imports
+from pyfile_parser import *
 
 
 def test_imports():
-    assert check_imports('sample_app.py') is True
+    assert check_imports(get_lines('sample_app.py')) is True
+
+def test_responses():
+    i_lines = [(i, line) for i, line in enumerate(get_lines('sample_app.py'))]
+    assert get_responses(i_lines) == ["return 'Hello, World!'"]
